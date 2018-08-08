9124f3a9464cfd2e0a7dcd24d01f849566454006
Adam Duff <j.adam.duff@gmail.com>
2018-08-08 04:45:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ var destructivelyAppendKitten = function(name) {
   kittens.push(name);
   return kittens;
 }
+
+var destructivelyPrependKitten = function(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+var destructivelyRemoveLastKitten = function(name) {
+  kittens.slice(0, kittens.length - 1);
+  return kittens;
+}
+
+var destructivelyRemoveFirstKitten = function(name) {
+  kittens.slice(1);
+  return kittens;
+}
+
+var appendKitten = function(name) {
+  return kittens.push(name);
+}
